b5732fd10faca72c1f28c65fb6055998b4da4526
Mohammad Sameer Almaghteh <modtech7677@gmail.com>
2023-08-05 23:30:21 +0300

    adding forgot password

---
+++ lib/src/widget/custom_Widget.dart/app_text.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+  final TextAlign? textAlign;
+
+  const AppText({
+    Key? key,
+    required this.text,
+    this.fontSize = 14,
+    this.fontWeight = FontWeight.w400,
+    this.color = const Color(0xff020953),
+    this.textAlign,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign == null ? null : TextAlign.center,
+      style: GoogleFonts.poppins(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        letterSpacing: 1.0.h,
+        color: color,
+        
+      ),
+    );
+  }
+}
