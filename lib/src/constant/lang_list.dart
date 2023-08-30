b94c1deab121feaa34af439b73a05606f918e1eb
ud-abdullah-alhamed <alhamedabdullah@outlook.com>
2023-08-30 03:34:50 +0300

    re

--- lib/src/constant/lang_list.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Languages {
-  static List locale = [
-    {'name': 'ENGLISH', 'locale': const Locale('en', 'US')},
-    {'name': 'ARABIC', 'locale': const Locale('ar', 'SA')},
-  ];
-}
